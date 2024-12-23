5f9577e7b94f3c515a1cdeb14cdaadb102c380ac
kto <1053753518@qq.com>
2024-12-23 17:23:12 +0800

    v1.0.0

--- ui/main.py
+++ ui/main.py
@@ -1,12 +1,11 @@
 import sys
-from abc import abstractmethod,ABC
 from pathlib import Path
 from PySide6.QtCore import QFile
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtWidgets import QWidget, QPushButton, QTextBrowser, QApplication, QMessageBox
 from loguru import logger
 
-class menuWindow(QWidget,ABC):
+class menuWindow(QWidget):
     def __init__(self, loader):
         super().__init__()
         self.loader = loader
@@ -17,10 +16,10 @@ class menuWindow(QWidget,ABC):
     def initialization(self):
         # 获取当前文件所在的目录
         base_dir = Path(__file__).resolve().parent
-        logger.debug(f"Base directory: {base_dir}")
+        logger.debug(f"基础目录: {base_dir}")
         # 构建UI文件的路径
         ui_file_path = base_dir / 'menu.ui'
-        logger.debug(f"UI file path: {ui_file_path}")
+        logger.debug(f"UI文件路径: {ui_file_path}")
         # 打开UI文件
         ui_file = QFile(str(ui_file_path))
         if not ui_file.open(QFile.ReadOnly):
@@ -42,7 +41,8 @@ class menuWindow(QWidget,ABC):
         self.ui.activateWindow()
         logger.info("UI窗口已显示并激活")
         self.bind_button()
-    @abstractmethod
+        logger.info("按钮绑定完成")
+
     def bind_button(self):
         # 定义按钮与方法的映射关系
         button_map = {
@@ -58,43 +58,51 @@ class menuWindow(QWidget,ABC):
                 logger.debug(f"按钮 '{button_name}' 已绑定到方法 '{handler.__name__}'")
             else:
                 logger.warning(f"未找到按钮: {button_name}")
-        logger.info("所有按钮绑定完成")
+
     def show_window(self):
         # 检查是否有ui属性且不为None
         if not hasattr(self, 'ui') or self.ui is None:
+            logger.warning("UI对象不存在或为None，无法显示窗口")
             return
         # 显示UI窗口
         self.ui.show()
         # 激活窗口，使其获得焦点
         self.ui.activateWindow()
+        logger.info("UI窗口已显示并激活")
 
     def quit_app(self):
         # 退出应用程序
         logger.info("退出应用程序")
         sys.exit()
+
     def console(self):
         text_browser = self.ui.findChild(QTextBrowser, 'textBrowser')
         return text_browser
+
     def button_clicked(self):
         self.console().append("按钮被点击了")
+        logger.info("按钮被点击")
+
 def main():
     try:
         # 创建UI加载器对象
         loader = QUiLoader()
+        logger.info("UI加载器对象创建完成")
 
         # 创建应用程序对象
         app = QApplication(sys.argv)
+        logger.info("应用程序对象创建完成")
 
         # 创建Stats对象，用于统计和展示数据
         stats = menuWindow(loader)
         stats.show_window()
+
         # 运行应用程序的主循环
         sys.exit(app.exec())
     except Exception as e:
         # 如果发生异常，退出程序，返回错误码1
         logger.exception("程序执行过程中发生异常")
         sys.exit(1)
-
 
 if __name__ == "__main__":
     main()
