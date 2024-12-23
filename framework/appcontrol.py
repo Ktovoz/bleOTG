0acbb31995da622166696d549a87aeb557777b2d
kto <1053753518@qq.com>
2024-12-23 16:56:01 +0800

    v1.0.0

--- framework/appcontrol.py
+++ framework/appcontrol.py
@@ -1,26 +1,61 @@
 import requests
-
 from configobj import ConfigObj
 from loguru import logger
+from PIL import Image
+from io import BytesIO
 
 class AppControl:
     def __init__(self, cfg):
-        ip=cfg.get('ip')
-        self.url=f'http://{ip}'
+        ip = cfg.get('ip')
+        self.url = f'http://{ip}:8080'
         logger.debug(f"已设置IP地址为{ip}")
         logger.debug(f"已设置请求地址为{self.url}")
-        pass
+
+    def contains(self, response, expected_text):
+        if expected_text in response.text:
+            logger.info(f"响应内容包含预期的文字'{expected_text}'，断言成功")
+            return True
+        else:
+            logger.error(f"响应内容不包含预期的文字'{expected_text}'")
+            return False
+
+    def not_contains(self, response, expected_text):
+        if expected_text not in response.text:
+            logger.info(f"响应内容不包含预期的文字'{expected_text}'，断言成功")
+            return True
+        else:
+            logger.error(f"响应内容包含预期的文字'{expected_text}'")
+            return False
+
     def test_bind(self):
-        endpoint=f'{self.url}/hello'
-        response= requests.get(endpoint)
+        endpoint = f'{self.url}/hello'
+        response = requests.get(endpoint)
         logger.debug(f"请求地址为{endpoint}")
-        logger.debug(f"已收到响应{response}")
 
+        if self.contains(response, "手机http服务器已正常启动"):
+            logger.info("测试链接成功")
 
-
+    def get_image(self):
+        endpoint = f'{self.url}/capimg'
+        response = requests.get(endpoint)
+        logger.debug(f"请求地址为{endpoint}")
+        if self.not_contains(response, "http response err"):
+            image = Image.open(BytesIO(response.content))
+            logger.debug(f"成功截取图片，格式为 {image.format}")
+            image.show()
+        else:
+            rep = requests.get(f'{self.url}/capimgpermission')
+            if self.contains(rep, "ok"):
+                logger.debug(f"截图权限申请成功")
+                image = Image.open(BytesIO(rep.content))
+                logger.debug(f"成功截取图片，格式为 {image.format}")
+                image.show()
+            else:
+                logger.error("截图失败，并且截图权限申请失败")
 
 if __name__ == '__main__':
-
-    cfg=ConfigObj('./../config.ini', encoding='utf8')
-    appcfg=cfg['app']
-    app=AppControl(appcfg)
+    cfg = ConfigObj('./../config.ini', encoding='utf8')
+    appcfg = cfg['app']
+    app = AppControl(appcfg)
+    app.test_bind()
+    app.get_image()
---
+++ readme.md
@@ -0,0 +1,21 @@
+
+
+
+
+更新解释器
+python.exe -m pip install --upgrade pip -i https://pypi.tuna.tsinghua.edu.cn/simple
+日志记录库
+pip install loguru  -i https://pypi.tuna.tsinghua.edu.cn/simple
+配置读取库
+pip install configobj  -i https://pypi.tuna.tsinghua.edu.cn/simple
+GUI库
+pip install pyside6  -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+使用pipdeptree查看依赖及子依赖
+pip install pipdeptree  -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+使用pipreqs查看项目所使用的依赖
+pip install pipreqs  -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+使用pip-autoremove 卸载未被使用的依赖
+pip install pip-autoremove  -i https://pypi.tuna.tsinghua.edu.cn/simple
